c81927556b81e2296717b62597a647e80a4eb264
yuzuchan05 <j.teee.05@gmail.com>
2024-11-02 16:01:18 -0700

    update

--- assets/script/main.js
+++ assets/script/main.js
@@ -10,3 +10,29 @@ $(document).ready(function () {
         $(".hamburger").fadeOut();
     });
 });
+
+
+/* 手動slider
+-----------------------------------------*/
+var mySwiper = new Swiper(".swiper", {
+    loop: true,
+    speed: 1000,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 76,
+    centeredSlides: false,
+    watchOverflow: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        bulletClass: 'swiper-pagination-bullet',
+        bulletActiveClass: 'swiper-pagination-bullet-active'
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 70,
+        },
+    },
+});
